0c869c2939ae011a51ad0b642a10d074da237853
Atakan <tepe_atakan@outlook.com>
2025-07-19 12:48:14 +0300

    add tailwind v4

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
-<template>
-  <div>Hello from vue-jsf! Test form here soon.</div>
-</template>
+<template> 
+<div class="min-h-screen bg-zinc-900 flex items-center justify-center">
+  <div class="p-4 max-w-lg w-full bg-zinc-800 rounded-lg shadow-lg">
+    <h1 class="text-2xl font-bold mb-4 text-zinc-50">Vue JSON Schema Form</h1>
+    <p class="mb-4 text-zinc-50">This is a simple Vue 3 JSON Schema Form generator with Tailwind CSS and Vuelidate support.</p>
+  </div>
+</div>
+  
+</template> 
+
 <script setup lang="ts"></script>
