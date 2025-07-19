8352817116dd9d369c3d11072bbf8c4865bc1435
Atakan <tepe_atakan@outlook.com>
2025-07-19 12:40:29 +0300

    initiate vue-jsf

---
+++ src/App.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>Hello from vue-jsf! Test form here soon.</div>
+</template>
+<script setup lang="ts"></script>
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue';
+export const SchemaForm = defineComponent({
+  name: 'SchemaForm',
+  props: { schema: Object },
+  setup() { return {}; },
+});
+
+export default SchemaForm; 
